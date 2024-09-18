035cc3f6750bb8d5b55d6257bf48cc6952d5783a
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-09-19 01:24:43 +0300

    Refactor category services in Node.js API

--- Node.js API/services/categories.services.js
+++ Node.js API/services/categories.services.js
@@ -1,6 +1,7 @@
+const { MONG0_DB_CONFIG } = require("../config/app.config");
 const { category } = require("../models/category.model");
 
-async function createCategory(params, callback) {
+async function createCategories(params, callback) {
     if (!params.categoryName) {
         return callback(
             {
@@ -15,6 +16,70 @@ async function createCategory(params, callback) {
         .save()
         .then((response) => {
             return callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+}
+
+async function getCategories(params, callback) {
+    const categoryName = params.categoryName;
+    var condition = categoryName ? 
+    {
+        categoryName:{$regex: new RegExp(categoryName), $options:"i"}
+    }
+    : {};
+    let perPage = Math.abs(params.pageSize) || MONG0_DB_CONFIG.PAGE_SIZE;
+    let page = (Math.abs(params.page) || 1) -1
+
+    category
+        .find(condition, "categoryName categoryImage")
+        .limit(perPage)
+        .skip(perPage * page)
+        .then((response) => {
+            return callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+}
+
+async function getCategoriesById(params, callback) {
+    const categoryId = params.categoryId;
+
+    category
+        .findById(categoryId)
+        .then((response) => {
+            if(!response) callback("Not found category with id" + categoryId);
+            else callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+}
+
+async function updateCategory(params, callback) {
+    const categoryId = params.categoryId;
+
+    category
+        .findByIdAndUpdate(categoryId, {useFindAndModify: false})
+        .then((response) => {
+            if(!response) callback("Not found category with id" + categoryId);
+            else callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+}
+
+async function DeleteCategory(params, callback) {
+    const categoryId = params.categoryId;
+
+    category
+        .findByIdAndDelete(categoryId)
+        .then((response) => {
+            if(!response) callback("Not found category with id" + categoryId);
+            else callback(null, response);
         })
         .catch((error) => {
             return callback(error);
