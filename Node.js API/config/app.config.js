40892c1af5338552ad42e009cd300eaec93e7338
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-09-18 21:25:32 +0300

    Add MongoDB configuration to app.config.js

---
+++ Node.js API/config/app.config.js
@@ -0,0 +1,8 @@
+const MONG0_DB_CONFIG = {
+    DB: "mongodb+srv://ahmeddeyiaa0212:<Iywf3n7jYQKN6M45>@cluster0.y7c91.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    PAGE_SIZE: 10,
+}
+
+module.exports = {
+    MONG0_DB_CONFIG,
+}
