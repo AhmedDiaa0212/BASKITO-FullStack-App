eb575876680cc1c32c62b6657e2db8861a1aa9cc
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-09-21 00:01:53 +0300

    Refactor config file to add constants for app name, API URL, category API, and page size

---
+++ baskito_app/lib/core/config.dart
@@ -0,0 +1,6 @@
+class Config {
+  static const String appName = "Baskito APP";
+  static const String apiUrl = "localhost:4000";
+  static const String categoryApi = "api/category";
+  static const int pageSize = 10;
+}
