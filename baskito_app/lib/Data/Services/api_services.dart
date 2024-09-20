10cec0e8ea9669c2d465267ba05456af00995995
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-09-21 02:44:26 +0300

    Refactor API services to fetch categories with pagination

--- baskito_app/lib/Data/Services/api_services.dart
+++ baskito_app/lib/Data/Services/api_services.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
-import 'package:baskito_app/Data/models/category.dart';
+import 'package:baskito_app/Data/models/category%20model/category.dart';
 import 'package:baskito_app/core/config.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
+
+final apiService = Provider((ref) => ApiServices());
 
 class ApiServices {
   static var client = http.Client();
@@ -19,6 +22,10 @@ class ApiServices {
 
     if (response.statusCode == 200) {
       var data = jsonDecode(response.body);
+
+      return categoriesFromJson(data["data"]);
+    } else {
+      return null;
     }
   }
 }
