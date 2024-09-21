66aa2d10b8cc5550c3703c5b2e0d5878159ee83f
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-09-21 19:51:59 +0300

    categoriesProvider file

---
+++ baskito_app/lib/Presentation/manager/category_provider.dart
@@ -0,0 +1,12 @@
+import 'package:baskito_app/Data/Services/api_services.dart';
+import 'package:baskito_app/Data/models/category%20model/category.dart';
+import 'package:baskito_app/Data/models/pagination%20model/pagination.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final categoriesProvider = FutureProvider.family<List<Category>?, Pagination>(
+  (ref, pagination) {
+    final apiRepository = ref.watch(apiService);
+
+    return apiRepository.getCategories(pagination.page, pagination.pageSize);
+  },
+);
