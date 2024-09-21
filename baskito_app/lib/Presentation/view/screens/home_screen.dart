b767f666d1a191040df497f4d43ada9795a210ac
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-09-22 00:46:12 +0300

    Refactor home_screen.dart to add a widget for displaying categories with pagination

--- baskito_app/lib/Presentation/view/screens/home_screen.dart
+++ baskito_app/lib/Presentation/view/screens/home_screen.dart
@@ -1,10 +1,45 @@
+import 'package:baskito_app/Presentation/view/widgets/home_categories.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsetsDirectional.all(8.0),
+              child: Text(
+                'Welcome to Baskito',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsetsDirectional.all(8.0),
+              child: Text(
+                'Your one stop shop for all your grocery needs',
+                style: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsetsDirectional.all(8.0),
+                child: ListView(
+                  children: const [HomeCategories()],
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
