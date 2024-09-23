811f0534b0f6e92f43749200ffcec3b4c0c95974
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-09-23 13:59:50 +0300

    Refactor package.json and index.js to add CORS middleware for enabling Cross-Origin Resource Sharing

--- Node.js API/index.js
+++ Node.js API/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const { MONG0_DB_CONFIG } = require("./config/app.config");
 const errors = require("./middleware/errors");
+const cors = require('cors');
 const swaggerUi = require("swagger-ui-express");
 swaggerDocument = require("./swagger.json");
 
@@ -23,8 +24,14 @@ app.use(express.json());
 app.use("./uploads", express.static("uploads"));
 app.use("/api", require("./routes/app.routes"));
 app.use(errors.errorHandler);
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.port || 4000, function () {
     console.log("Ready to Go!")
 });
+
+
