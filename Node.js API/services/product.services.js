425d966cf87349beb1248079289238b05e56d499
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-09-23 15:32:49 +0300

    Refactor product.services.js to add CRUD operations for products

---
+++ Node.js API/services/product.services.js
@@ -0,0 +1,115 @@
+const { MONG0_DB_CONFIG } = require("../config/app.config");
+const { product } = require("../models/product.model");
+const { category } = require("../models/category.model");
+
+async function createProduct(params, callback) {
+    if (!params.productName) {
+        return callback(
+            {
+                message: "Product Name Required"
+            },
+            ""
+        );
+    }
+
+    if (!params.category) {
+        return callback(
+            {
+                message: "Category Required"
+            },
+            ""
+        );
+    }
+
+    const productModel = new product(params);
+    productModel
+        .save()
+        .then((response) => {
+            return callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+}
+
+async function getProducts(params, callback) {
+    const productName = params.productName;
+    const categoryId = params.categoryId;
+    var condition = {};
+
+    if (productName) {
+        condition["productName"] =
+            { productName: { $regex: new RegExp(productName), $options: "i" } };
+    }
+
+    if (categoryId) {
+        condition["categoryId"] = categoryId;
+    }
+
+    let perPage = Math.abs(params.pageSize) || MONG0_DB_CONFIG.PAGE_SIZE;
+    let page = (Math.abs(params.page) || 1) - 1
+
+    product
+        .find(condition, "productId productName productShortDescription  productPrice  productSalePrice productImage productSKU productType stockStatus")
+        .populate("category", "categoryName categoryImage")
+        .limit(perPage)
+        .skip(perPage * page)
+        .then((response) => {
+            return callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+}
+
+async function getProductById(params, callback) {
+    const productId = params.productId;
+
+    product
+        .findById("productId")
+        .populate("category", "categoryName categoryImage")
+        .then((response) => {
+            return callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+}
+
+async function updateProduct(params, callback) {
+    const productId = params.productId;
+
+    product
+        .findByIdAndUpdate("productId", params, { useFindAndModify: false })
+        .then((response) => {
+            if (!response) 
+                callback("Not found product with id" + productId);
+            else callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+}
+
+async function deleteProduct(params, callback) {
+    const productId = params.productId;
+
+    product
+        .findByIdAndRemve("productId")
+        .then((response) => {
+            if (!response) 
+                callback("Not found product with id" + productId);
+            else callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+}
+
+module.exports = {
+    createProduct,
+    getProducts,
+    getProductById,
+    updateProduct,
+    deleteProduct
+  }
