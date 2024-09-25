5ddd7bee07dc052698d23b10e665776533366843
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-09-25 17:47:34 +0300

    Refactor related-products.services.js to include functions for adding and removing related products

---
+++ Node.js API/services/related-products.services.js
@@ -0,0 +1,64 @@
+const { product } = require("../models/product.model");
+const { relatedProduct } = require("../models/related-products.model"); 
+
+async function addRelatedProducts(params, callback) {
+    if (!params.product) {
+        return callback(
+            {
+                message: "Product Id Required"
+            },
+            ""
+        );
+    }
+
+    if (!params.relatedProduct) {
+        return callback(
+            {
+                message: "Related Product Id Required"
+            },
+            ""
+        );
+    }
+
+    const relatedProductModel = new relatedProduct(params); 
+    relatedProductModel
+        .save()
+        .then(async (response) => {
+            await product.findOneAndUpdate(
+                {
+                    _id: params.product
+                },
+                {
+                    $addToSet: {
+                        "relatedProducts": relatedProductModel // Make sure this matches schema
+                    }
+                },
+            );
+            return callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+}
+
+async function removeRelatedProducts(params, callback) {
+    const id = params.id;
+
+    relatedProductModel.findByIdAndDelete(id)
+        .then((response) => {
+            if(!response){
+                callback("Product Id Not Found");   
+            } 
+            else{
+                callback(null, response);
+            }
+        })
+        .catch((error) => {
+            return callback(error);
+        });
+}
+
+module.exports ={
+    addRelatedProducts,
+    removeRelatedProducts
+}
