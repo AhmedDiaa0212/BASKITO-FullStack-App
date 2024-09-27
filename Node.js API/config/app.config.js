63c7c37999afd81d044e859b14fe464766d5d198
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-09-27 11:32:39 +0300

    update

--- Node.js API/config/app.config.js
+++ Node.js API/config/app.config.js
@@ -3,12 +3,6 @@ const MONG0_DB_CONFIG = {
     PAGE_SIZE: 10,
 }
 
-const STRIPE_CONFIG = {
-    STEIPE_KEY: "sk_test_51Q35OuKToyL8NFNDTBUgpvhoUvGGJq6j050r74qH9rTnSf5o4NR6ysaRvbYSuwCh8XhQNrSq6s24GZrBCSwIYjRB00TyVRYqkq",
-    CURRENCY: "inr",
-}
-
-
 module.exports = {
     MONG0_DB_CONFIG,
     STRIPE_CONFIG
